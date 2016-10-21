202f90208d7cae42ac9fbee26457a1788cc74d78
memorylose <lucifering@126.com>
2016-10-21 17:04:26 +0800

    add files

---
+++ Vera.Business/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Vera.Interface.BLL;
+
+namespace Vera.Business
+{
+    public class UserRepository : IUserRepository
+    {
+        private IUserRepository users;
+        public UserRepository(IUserRepository _users)
+        {
+            users = _users;
+        }
+
+        public bool Login(string username, string password)
+        {
+            return users.Login(username, password);
+        }
+    }
+}
