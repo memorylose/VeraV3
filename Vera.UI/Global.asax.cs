9d483ec6d8b971ff7dbb2d81891c19f1df388f46
memorylose <lucifering@126.com>
2016-10-21 17:12:48 +0800

    add DI

--- Vera.UI/Global.asax.cs
+++ Vera.UI/Global.asax.cs
@@ -1,17 +1,32 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Integration.Web;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
+using Vera.Business;
+using Vera.DataAccess.AdoNet;
+using Vera.Interface.BLL;
+using Vera.Interface.DAL;
 
 namespace Vera.UI
 {
-    public class Global : System.Web.HttpApplication
+    public class Global : System.Web.HttpApplication, IContainerProviderAccessor
     {
+        static IContainerProvider _containerProvider;
+        public IContainerProvider ContainerProvider
+        {
+            get { return _containerProvider; }
+        }
+
         protected void Application_Start(object sender, EventArgs e)
         {
-
+            var builder = new ContainerBuilder();
+            builder.RegisterType<UserDal>().As<IUserDataAccess>();
+            builder.RegisterType<UserRepository>().As<IUserRepository>();
+            _containerProvider = new ContainerProvider(builder.Build());
         }
     }
 }
