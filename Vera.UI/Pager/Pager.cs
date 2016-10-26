567d90a14c0d03c3d0082a4c0e4872981d86677d
memorylose <lucifering@126.com>
2016-10-26 16:00:47 +0800

    add pager class

---
+++ Vera.UI/Pager/Pager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vera.UI.Pager
+{
+    public class Pager
+    {
+        public void GetPager(int totalCount, int pageSize)
+        {
+
+        }
+    }
+}
