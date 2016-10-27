bbbc814375088d7c34814a894b5f601e56616f3d
memorylose <lucifering@126.com>
2016-10-27 18:00:09 +0800

    update pager.

--- Vera.UI/Pager/Pager.cs
+++ Vera.UI/Pager/Pager.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace Vera.UI.Pager
 {
     public class Pager
     {
-        public void GetPager(int totalCount, int pageSize)
+        public void GetPager(int totalCount, int pageSize, int currentPage)
         {
+            decimal totalPager = Math.Ceiling(Convert.ToDecimal(totalCount / pageSize));
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 1; i <= totalPager; i++)
+            {
+                if (currentPage == 0)
+                {
+                    strBuilder.Append("<div class=\"pager - e\">1</div>");
 
+                }
+            }
         }
     }
 }
