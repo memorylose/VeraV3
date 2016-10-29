ff5ff7d29c6eaf97602076f5213804239a06731d
memorylose <lucifering@126.com>
2016-10-29 21:48:20 +0800

    update

---
+++ Vera.UI/Articles.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Vera.Interface.BLL;
+
+namespace Vera.UI
+{
+    public partial class Articles : System.Web.UI.Page
+    {
+        public IArticleRepository articleRep { get; set; }
+        public string Title;
+        public string CrDate;
+        public string TypeName;
+        public string Contents;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetArticleDetail(Convert.ToInt32(Request.QueryString["id"]));
+        }
+
+        private void GetArticleDetail(int articleId)
+        {
+            var articleDetail = articleRep.GetArticleDetail(articleId);
+            Title = articleDetail.Title;
+            CrDate = articleDetail.CreateDate.ToString(); ;
+            TypeName = articleDetail.TypeName;
+            Contents = articleDetail.Contents;
+        }
+    }
+}
