4525aab816614172b52a73cc766c205888e2a2aa
memorylose <lucifering@126.com>
2016-11-11 16:48:54 +0800

    update test case

---
+++ Vera.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vera.Business;
+using Moq;
+using Vera.Interface.DAL;
+using Vera.Model;
+
+namespace Vera.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidationLoginInformationWithUserNameEmpty()
+        {
+            var userData = new Mock<IUserDataAccess>();
+            UserRepository userRep = new UserRepository(userData.Object);
+
+            User userModel = new User() { UserName = "" };
+
+            string result = userRep.ValidateLogin(userModel);
+        }
+    }
+}
